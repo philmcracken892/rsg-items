801d2665e98df9335196b08cd5d064ec55e1a46a
philmcracken892 <MARC.KELLETT@GOOGLEMAIL.COM>
2025-01-30 18:02:09 +0000

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -4,7 +4,7 @@ RegisterNetEvent('rewards:GiveItem', function(itemName)
     local src = source
     local Player = RSGCore.Functions.GetPlayer(src)
     
-    -- Add item to player's inventory
+    
     Player.Functions.AddItem(itemName, 1)
     TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items[itemName], 'add')
 end)
